63e5f9c3a157f5e49cf68ba82b14157c3bf2b5db
Nicola Martinez <nicoenrique_1994@hotmail.com>
2021-10-06 22:05:13 -0300

    catch values from form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import '../styles/login.css';
 const Login = () => {
+    const [LoginData, setLoginData] = useState({
+        email:"",
+        password:"",
+    });
+    const HandleChange = (e) =>{
+        setLoginData({ ...LoginData, [e.target.name]: e.target.value });
+    } 
     return <div className="container">
         <div className="bodyshape mx-auto mt-auto">
             <form className="row needs-validation mx-auto login-form" noValidate>
@@ -8,14 +15,14 @@ const Login = () => {
                     <div className="col-md-10 mb-2 mx-auto">
                         <label className="form-label">Email</label>
                         <div className="input-group has-validation">
-                            <input type="text" className="form-control" id="EmailLogin" placeholder="Email" required />
+                            <input type="text" className="form-control" id="EmailLogin" placeholder="Email" name="email" onChange={HandleChange}  value={LoginData.email} required />
                         </div>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-md-10 mb-2 mx-auto">
                         <label className="form-label">Password</label>
-                        <input type="Password" className="form-control" placeholder="password" id="Password" required />
+                        <input type="Password" className="form-control" placeholder="password" id="Password" name="password" onChange={HandleChange} value={LoginData.password} required />
                     </div>
                 </div>
                 <div className="row">
