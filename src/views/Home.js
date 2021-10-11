a5bc07bd37f38c64d941d855f6135e7dffe07afc
Nicola Martinez <nicoenrique_1994@hotmail.com>
2021-10-11 15:58:04 -0300

    register and login form working perfectly with backend

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,12 +1,12 @@
 import React from "react";
-import baner from '../img/baner.png';
+import banner from '../img/banner.png';
 
 const Home = () => {
     return (
         <>
             <div className="row backgroundimg ">
                 <div className="col-md-12">
-                    <img src={baner} className="img-fluid" alt="Responsive image" />
+                    <img src={banner} className="img-fluid" alt="Responsive image" />
                 </div>
             </div>
             <div className="col-md-12 d-flex align-items-center">
--- src/views/Register_Alumn.js
+++ src/views/Register_Alumn.js
@@ -1,18 +1,14 @@
 import React, { useState } from "react";
-import baner from '../img/baner.png';
+import banner from '../img/banner.png';
 import anom from '../img/anom.png';
 import { useHistory } from "react-router-dom";
 
 const RegisterAlumn = () => {
     const [Profile, setProfile] = useState({
-        name: "",
-        lastname: "",
+        full_name: "",
+        last_name: "",
         email: "",
-        password: "",
-        question: "",
-        answer: "",
-        knowledge: "",
-        role: "",
+        password: ""
 
     });
     const history = useHistory();
@@ -21,20 +17,16 @@ const RegisterAlumn = () => {
     }
     const onSubmit = (e) => {
         e.preventDefault();
-        fetch("http://localhost:8080/Profile", {
-            method: 'PUT',
+        fetch("http://localhost:8080/profile", {
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                name: Profile.name,
-                lastname: Profile.lastname,
+                full_name: Profile.full_name,
+                last_name: Profile.last_name,
                 email: Profile.email,
                 password: Profile.password,
-                question: Profile.question,
-                answer: Profile.answer,
-                knowledge: Profile.knowledge,
-                role: "student",
             })
         }).then(resp => {
             if (resp.status === 200) {
@@ -50,7 +42,7 @@ const RegisterAlumn = () => {
     return (
         <>
             <div className="col-md-12">
-                <img src={baner} className="img-fluid" alt="Responsive image" />
+                <img src={banner} className="img-fluid" alt="Responsive image" />
             </div>
             <div className="row">
                 <div className="col-md-2 p-2">
@@ -60,11 +52,11 @@ const RegisterAlumn = () => {
                     <form onSubmit={onSubmit}>
                         <div className="mb-3">
                             <label for="Name" className="form-label">Name</label>
-                            <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="name" onChange={HandleChange} value={Profile.name} />
+                            <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="full_name" onChange={HandleChange} value={Profile.full_name} />
                         </div>
                         <div className="mb-3">
                             <label for="Last Name" className="form-label">Last Name</label>
-                            <input type="text" className="form-control" id="exampleInputPassword1" onChange={HandleChange} name="lastname" value={Profile.lastname} />
+                            <input type="text" className="form-control" id="exampleInputPassword1" onChange={HandleChange} name="last_name" value={Profile.last_name} />
                         </div>
                         <div className="mb-3">
                             <label for="Email" className="form-label">Email</label>
@@ -72,28 +64,8 @@ const RegisterAlumn = () => {
                             <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                         </div>
                         <div className="mb-3">
-                            <label for="Repeat Email" className="form-label">Repeat Email</label>
-                            <input type="text" className="form-control" id="exampleInputPassword1" />
-                        </div>
-                        <div className="mb-3">
                             <label for="Pass" className="form-label">Password</label>
                             <input type="password" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="password" onChange={HandleChange} value={Profile.password} />
-                        </div>
-                        <div className="mb-3">
-                            <label for="Repeat Pass" className="form-label">Repeat Password</label>
-                            <input type="password" className="form-control" id="exampleInputPassword1" />
-                        </div>
-                        <div className="mb-3">
-                            <label for="Repeat Email" className="form-label">Security Question</label>
-                            <input type="text" className="form-control" id="exampleInputPassword1" onChange={HandleChange} name="question" value={Profile.question} />
-                        </div>
-                        <div className="mb-3">
-                            <label for="Answer" className="form-label">Answer</label>
-                            <input type="text" className="form-control" id="exampleInputPassword1" onChange={HandleChange} name="answer" value={Profile.answer} />
-                        </div>
-                        <div className="mb-3">
-                            <label for="Repeat Email" className="form-label">Knowledge</label>
-                            <input type="text" className="form-control" id="exampleInputPassword1" onChange={HandleChange} name="knowledge" value={Profile.knowledge} />
                         </div>
                         <button type="submit" className="btn btn-primary">Submit</button>
                     </form>
