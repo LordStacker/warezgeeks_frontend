deb9c6b95178c7d1f644b634b767a37f73667b83
Nicola Martinez <nicoenrique_1994@hotmail.com>
2021-11-06 22:06:27 -0300

    logout and token done

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,7 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-const Dashboard = () =>{
-    return <h1>Hola testing history</h1>
+const Dashboard = () => {
+    const [user, setUser] = useState("");
+    useEffect(() => {
+        fetch("http://localhost:8080/me", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                'Authorization': `Bearer ${JSON.parse(localStorage.getItem("access_token"))}`
+            },
+            body: JSON.stringify("")
+        })
+            .then(resp => resp.json())
+            .then((data) => {
+                setUser(data.current_user)
+            })
+            .catch(error => {
+                console.error(error);
+            })
+    }, [])
+    return <h1>Hola {user} history</h1>
 }
 
 export default Dashboard;
